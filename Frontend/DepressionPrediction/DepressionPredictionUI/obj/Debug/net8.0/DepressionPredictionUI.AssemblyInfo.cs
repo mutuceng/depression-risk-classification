92f2f9291b2a219ff39cb14f22ca7943e7aaa235
Ulaş Uçan <uls.ucn@gmail.com>
2025-01-03 11:56:22 +0300

    last push

--- Frontend/DepressionPrediction/DepressionPredictionUI/obj/Debug/net8.0/DepressionPredictionUI.AssemblyInfo.cs
+++ Frontend/DepressionPrediction/DepressionPredictionUI/obj/Debug/net8.0/DepressionPredictionUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DepressionPredictionUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612dd063229b3d076dd5038d9dd357a03dde1718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a5a260b8c7c165d830a05c31b53278c012ae19")]
 [assembly: System.Reflection.AssemblyProductAttribute("DepressionPredictionUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DepressionPredictionUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
